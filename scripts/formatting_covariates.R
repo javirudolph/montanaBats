e4040319a862222823ae0d97ccf4c25aeb0572c0
javirudolphair <f.javierarudolph@gmail.com>
2023-01-13 19:39:05 -0500

    successfully joined these two spatial datasets! jurisdictions and covariates in the same one at the NABat grid level.

---
+++ scripts/formatting_covariates.R
@@ -0,0 +1,54 @@
+#
+# MONTANA COVARIATES FOR GRID CELLS
+# SCRIPT BY F. JAVIERA RUDOLPH
+# QUESTIONS: f.javierarudolph at gmail.com
+# LAST UPDATED: January 13, 2023
+
+
+# Building a data frame with information on covariates for each of the grid cells in Montana
+
+# libraries
+library(sf)
+library(dplyr)
+library(ggplot2)
+
+# Join jurisdictions info with the montana covariates and the NABat grids
+mt_jurisdictions <- readRDS("datafiles/jurisdictions/mt_jurisdictions.RDS")
+
+# NABAT covariates
+# source: https://www.sciencebase.gov/catalog/item/620e6f3bd34e6c7e83baa603
+
+NABat_covariates <- read_sf("datafiles/nabat_covariates/NABat_grid_covariates/NABat_grid_covariates.shp") %>%
+  filter(., admin1 == "Montana") %>% 
+  select(GRTS_ID, karst, p_forest, p_wetland, mean_temp,
+         precip, DEM_max, physio_div, dist_mines, starts_with("eco"))
+
+# change crs to match the jurisdiction data
+mt_covs_NAD83 <- st_transform(NABat_covariates, st_crs(mt_jurisdictions))
+
+juris_list <- unique(mt_jurisdictions$jurisdiction)
+jusis_list_sub <- juris_list[c(5, 7, 10, 13, 17, 18, 20)]
+focus_jurisdictions <- mt_jurisdictions %>% filter(jurisdiction %in% jusis_list_sub)
+
+mt_covariates <- st_join(mt_covs_NAD83, focus_jurisdictions, largest = TRUE)
+
+# CHECKS
+# ecoregions
+
+ggplot(data = mt_covariates) + geom_sf(aes(fill = eco1_name))
+ggplot(data = mt_covariates) + geom_sf(aes(fill = eco2_name))
+ggplot(data = mt_covariates) + geom_sf(aes(fill = eco3_name))
+
+# jurisdictions
+ggplot(data = mt_covariates) + geom_sf(aes(fill = jurisdiction))
+
+# environmental variables
+ggplot(data = mt_covariates) + geom_sf(aes(fill = as.factor(karst)))
+ggplot(data = mt_covariates) + geom_sf(aes(fill = p_forest))
+ggplot(data = mt_covariates) + geom_sf(aes(fill = p_wetland))
+ggplot(data = mt_covariates) + geom_sf(aes(fill = mean_temp))
+ggplot(data = mt_covariates) + geom_sf(aes(fill = precip))
+ggplot(data = mt_covariates) + geom_sf(aes(fill = DEM_max))
+ggplot(data = mt_covariates) + geom_sf(aes(fill = physio_div))
+ggplot(data = mt_covariates) + geom_sf(aes(fill = dist_mines))
+
--- scripts/readme_jurisdictions.R
+++ scripts/readme_jurisdictions.R
@@ -1,29 +1,40 @@
+#
+# MONTANA JURISDICTIONS SPATIAL DATA
+# SCRIPT BY F. JAVIERA RUDOLPH
+# QUESTIONS: f.javierarudolph at gmail.com
+# LAST UPDATED: January 13, 2023
+
 # Working on this script to show where I downloaded data to set jurisdictions
-
-# the folder 'MTPublicLands_SHP' has the shapefiles for the different jurisdictions
-# I downloaded it from the Montana state library
-# link: https://mslservices.mt.gov/Geographic_Information/Data/DataList/datalist_Details.aspx?did=%7B60b5a8b0-b272-11e2-9e96-0800200c9a66%7D
-
 
 # libraries
 library(sf)
 library(dplyr)
 library(ggplot2)
-library(maps)
+
+# the folder 'MTPublicLands_SHP' has the shapefiles for the different jurisdictions
+# I downloaded it from the Montana state library
+# link: https://mslservices.mt.gov/Geographic_Information/Data/DataList/datalist_Details.aspx?did=%7B60b5a8b0-b272-11e2-9e96-0800200c9a66%7D
 
 # load the shapefiles
 
-mtpublands <- read_sf("datafiles/jurisdictions/MTPublicLands_SHP/Montana_PublicLands/Montana_PublicLands.shp")
+raw_mtpublands <- read_sf("datafiles/jurisdictions/MTPublicLands_SHP/Montana_PublicLands/Montana_PublicLands.shp")
 
-# Get state outline
-states <- st_as_sf(map("state", plot = FALSE, fill = TRUE))
-states_mt <- states %>% filter(ID == "montana")
+# getting a warning
+self_inter <- raw_mtpublands %>% 
+  mutate(validity = st_is_valid(raw_mtpublands, reason = TRUE)) %>% 
+  filter(validity != "Valid Geometry")
 
-# look at all of them. heavy plot, takes a while
-mtpublands %>% 
+# compare:
+self_inter %>% 
+  ggplot() + 
+  geom_sf()
+
+st_make_valid(self_inter) %>% 
   ggplot() +
-  geom_sf(data = states_mt) +
-  geom_sf(aes(fill = OWNER, color = OWNER))
+  geom_sf()
+
+# make those geometries valid again:
+mtpublands <- st_make_valid(raw_mtpublands)
 
 # To visualize independent jurisdictions
 owners <- mtpublands$OWNER %>%unique()
@@ -32,7 +43,6 @@ j<-18
 mtpublands %>% 
   filter(OWNER == owners[j]) %>% 
   ggplot() +
-  geom_sf(data = states_mt) +
   geom_sf(fill = "darkslateblue", color = "darkslateblue") +
   labs(title = owners[j])
 
@@ -40,32 +50,47 @@ mtpublands %>%
 # Found a different shape file:
 # https://mslservices.mt.gov/Geographic_Information/Data/DataList/datalist_Details.aspx?did={341205DA-7668-4119-9D21-0D1C8AFCF5F1}
 
+# Map the tribal jurisdictions
+
 mtreservations <- read_sf("datafiles/jurisdictions/MontanaReservations_shp/MontanaReservations.shp")
 
-mtreservations %>% 
+st_transform(mt_covariates, st_crs(mtreservations)) %>% 
+  st_join(., mtreservations) %>%
   ggplot() +
-  geom_sf(data = states_mt) +
   geom_sf(aes(fill = NAME, color = NAME))
 
 
-# Create a single dataframe with the geometries of interest
-our_jurisd <- owners[c(5, 7, 10, 13, 17, 18)]
+
+# Put these together in the same thing:
+# check they have the same crs
+st_crs(mtpublands)
+st_crs(mtreservations)
 
 mtpublands %>% 
   select(OWNER, geometry) %>% 
-  janitor::clean_names() %>% 
-  filter(owner %in% our_jurisd) %>% 
-  rename(jurisd = owner) -> A
+  rename(jurisdiction = OWNER) -> A
 
+# not sure if we need to focus on each independent reservation
+# for now, I am not
 mtreservations %>% 
-  select(geometry) %>% 
-  mutate(jurisd = 'Reservations') -> B
+  mutate(jurisdiction = "Reservations") %>% 
+  select(jurisdiction, geometry) -> B
 
-bind_rows(A, B) -> C
+mt_jurisdictions <- rbind(A, B)
+saveRDS(mt_jurisdictions, file = "datafiles/jurisdictions/mt_jurisdictions.RDS")
 
-C %>% 
-  ggplot() +
-  geom_sf(data = states_mt) +
-  geom_sf(aes(fill = jurisd, color = jurisd))
 
-saveRDS(C, file = "datafiles/jurisdictions/merged_jurisdictions.RDS")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
