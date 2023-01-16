ad9bacdfb58c3c8b909b568e48b8bf2c5ae7116a
javirudolphair <f.javierarudolph@gmail.com>
2023-01-16 11:17:50 -0500

    changing colors

---
+++ scripts/themes.R
@@ -0,0 +1,40 @@
+# 
+# Place to store themes and color palettes, etc.
+
+library(scales)
+
+my_pal <- c(         "#003B4F", 
+                     "#657422", 
+                     "#4758AB", 
+                     "#20794D", 
+                     "#5E5E5E", 
+                     "#AD0000",
+                     "#111111",
+                     "#00769E",
+                     "#8f5902")
+show_col(my_pal)
+  
+my_pal2 <- c(  "#f8f9fa", 
+               "#ecf0f1", 
+               "#dee2e6", 
+               "#ced4da", 
+               "#b4bcc2", 
+               "#95a5a6", 
+               "#7b8a8b", 
+               "#343a40", 
+               "#212529"
+)
+show_col(my_pal2)
+  
+my_pal3 <- c("#2c3e50", 
+"#6610f2", 
+"#6f42c1", 
+"#e83e8c", 
+"#e74c3c", 
+"#fd7e14", 
+"#f39c12", 
+"#18bc9c", 
+"#20c997", 
+"#3498db")
+
+show_col(my_pal3)
