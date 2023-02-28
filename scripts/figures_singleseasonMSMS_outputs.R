9faa6aa95aa37db20c4f6d9cad4d289672d0a14f
javirudolphair <f.javierarudolph@gmail.com>
2023-02-28 11:16:50 -0500

    very slowly building these figures

--- scripts/figures_singleseasonMSMS_outputs.R
+++ scripts/figures_singleseasonMSMS_outputs.R
@@ -4,12 +4,36 @@
 
 #   Code written by F. Javiera Rudolph, Ph.D.
 
+
+# libraries
 library(tidyverse)
 library(cowplot)
 library(sf)
 
 
-
+# load model outputs
 out_null_msms <- readRDS(file = "modelouts/out_null_msms.RDS")
 out_M1_msms <- readRDS(file = "modelouts/out_M1_msms.RDS")
 out_M2_msms <- readRDS(file = "modelouts/out_M2_msms.RDS")
+
+
+# Extract estimated values of psi and r for the three models and viz 
+
+# M1 betas
+m1_psi <- tibble(
+  covar = c("elev", "elev_sqrd", "temp", "temp_sqrd", "physdiv", "precip", "forest", "wetlands"),
+  means = out_M1_msms$mean$beta.lpsi,
+  sds = out_M1_msms$sd$beta.lpsi
+  )
+
+m1_psi %>% 
+  ggplot(., aes(x = means, y = covar)) +
+  geom_point() +
+  geom_errorbarh(aes(xmin = means - sds, xmax = means + sds))
+
+# create figures
+
+
+
+# Make Montana map with medians for bat calls for each of the cells sampled
+# compare that map to a prediction using the model for all of montana
