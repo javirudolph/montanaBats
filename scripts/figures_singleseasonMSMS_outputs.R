ab0bd5b8bb282ffa4584676f239b89758470f283
javirudolphair <f.javierarudolph@gmail.com>
2023-02-28 11:32:42 -0500

    made some figures

--- scripts/figures_singleseasonMSMS_outputs.R
+++ scripts/figures_singleseasonMSMS_outputs.R
@@ -19,19 +19,70 @@ out_M2_msms <- readRDS(file = "modelouts/out_M2_msms.RDS")
 
 # Extract estimated values of psi and r for the three models and viz 
 
-# M1 betas
-m1_psi <- tibble(
+
+
+psi_and_r <- bind_rows(c("psi", out_null_msms$mean$psi, out_null_msms$sd$psi))
+
+
+
+c(out_null_msms$mean$psi, out_null_msms$sd$psi)
+
+# oops, didn't track these in the models
+c(out_M1_msms$mean$psi, out_M1_msms$sd$psi)
+c(out_M2_msms$mean$psi, out_M2_msms$sd$psi)
+
+# Betas for psi
+m1_betas_psi <- tibble(
   covar = c("elev", "elev_sqrd", "temp", "temp_sqrd", "physdiv", "precip", "forest", "wetlands"),
   means = out_M1_msms$mean$beta.lpsi,
-  sds = out_M1_msms$sd$beta.lpsi
+  sds = out_M1_msms$sd$beta.lpsi,
+  model = "M1"
   )
 
-m1_psi %>% 
-  ggplot(., aes(x = means, y = covar)) +
-  geom_point() +
-  geom_errorbarh(aes(xmin = means - sds, xmax = means + sds))
+m2_betas_psi <- tibble(
+  covar = c("elev", "elev_sqrd", "temp", "temp_sqrd", "physdiv", "precip", "forest", "wetlands"),
+  means = out_M2_msms$mean$beta.lpsi,
+  sds = out_M2_msms$sd$beta.lpsi,
+  model = "M2"
+)
 
-# create figures
+
+betas_psi_df <- bind_rows(m1_betas_psi, m2_betas_psi)
+
+betas_psi_df %>% 
+  ggplot(., aes(x = means, y = covar, color = model)) +
+  geom_point() +
+  geom_errorbarh(aes(xmin = means - sds, xmax = means + sds)) +
+  theme_bw() -> psi_fig
+
+
+# Betas for r
+
+m1_betas_r <- tibble(
+  covar = c("karst", "forest", "physdiv"),
+  means = out_M1_msms$mean$beta.lr,
+  sds = out_M1_msms$sd$beta.lr,
+  model = "M1"
+)
+
+m2_betas_r <- tibble(
+  covar = c("karst", "forest", "physdiv"),
+  means = out_M2_msms$mean$beta.lr,
+  sds = out_M2_msms$sd$beta.lr,
+  model = "M2"
+)
+
+
+betas_r_df <- bind_rows(m1_betas_r, m2_betas_r)
+
+betas_r_df %>% 
+  ggplot(., aes(x = means, y = covar, color = model)) +
+  geom_point() +
+  geom_errorbarh(aes(xmin = means - sds, xmax = means + sds)) +
+  theme_bw() -> r_fig
+
+
+plot_grid(psi_fig + theme(legend.position = "none"), r_fig, rel_widths = c(0.85, 1))
 
 
 
